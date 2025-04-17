c57a3ccc13142462f8631648d3412848cfdd36bc
susannevanlaarhoven <susanne.van.laarhoven@textielmuseum.nl>
2025-04-17 12:17:27 +0200

    Update chapter 2 question 3.py

--- chapter 2 question 3.py
+++ chapter 2 question 3.py
@@ -1,6 +1,5 @@
 # Write a Tipper program where the user enters a restaurant bill total. The program should then display two amounts:
 #a 15 percent tip and a 20 percent tip.
-
-totalbill = float(input("Please enter the bill total: "))
-print("Your bill total including a 15% tip is: ", totalbill*1.15)
-print("Your bill total including a 20% tip is: ", totalbill*1.2)
+bill = float(input("Please enter the bill total: "))
+print("Your bill total including a 15% tip is: ", round((bill*1.15),2))
+print("Your bill total including a 20% tip is: ", round((bill*1.2),2))
