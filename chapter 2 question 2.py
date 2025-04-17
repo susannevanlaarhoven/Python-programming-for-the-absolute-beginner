4c97954c89688f5a5e6c8f80d572cb25bd44cbb4
susannevanlaarhoven <susanne.van.laarhoven@textielmuseum.nl>
2025-04-17 11:18:55 +0200

    Update chapter 2 question 2.py

--- chapter 2 question 2.py
+++ chapter 2 question 2.py
@@ -1,4 +1,7 @@
-# Write your code here :-)
+# Write a program that allows a user to enter his or her two
+favorite foods. The program should then print out the name of
+a new food by joining the original food names together.
+
 food1 = input("What is your first favourite food? ")
 food2 = input("What is your second favourite food? ")
 print(food1+food2)
